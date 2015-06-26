6f51aa3972b1104e9b7cb9e91957f45e5bfbdd4f
Paviel <forcu2mblog@gmail.com>
2015-06-26 18:12:34 +0300

    A

--- js/script.js
+++ js/script.js
@@ -1,27 +1,63 @@
 
 $(document).ready(function() {
 
-  $(window).resize(function() {
+  function slidersInit() {
 
-    createSliders();
-    
-  })
+    if( $(window).width() > 590 ) {
 
-  createSliders();
+      $(".slider--benefits__table_wrapper").css({"transform":"translateX(0)"});
 
-  function createSliders() {
+    }
 
-    if( $(window).width() < 590 ) {
+    if ( $(window).width() < 600 ) {
 
       $(".slider--benefits__table_wrapper").css({"transform":"translateX(-256px)"})
       $("#benefits_slider .slider_pagination a").removeClass("active");
       $("#benefits_slider .slider_pagination a:nth-child(2)").addClass("active");
 
+      $("#benefits_slider .slider_pagination a").click(function(event) {
+
+        event.preventDefault();
+
+        var parent = $(this).closest("#benefits_slider").find(".slider--benefits__table_wrapper");
+        var paginationLinks = $("#benefits_slider .slider_pagination a");
+        var count = $.inArray( this, paginationLinks );
+        var slides = $("#benefits_slider .slider__slide");
+        var width = $("#benefits_slider .slider__slide").css("width") + "px";
+        var slideTarget = $("#benefits_slider .slider__slide").get(count);
+
+        if ( !$(this).hasClass("active") ) {
+
+          $(paginationLinks).removeClass("active");
+          $(this).addClass("active");
+
+          if ( count == 0 ) {
+             $(parent).css({"transform":"translateX(21px)"})
+          }
+
+          if ( count == 1 ) {
+             $(parent).css({"transform":"translateX(-256px)"})
+          }
+
+          if ( count == 2 ) {
+             $(parent).css({"transform":"translateX(-533px)"})
+          }
+
+        }
+
+      })
+
+    }
+
+    if( $(window).width() < 1200 ) {
+
+      $("#review_slider .slider__slide").removeClass("active");
+
       $("#review_slider .slider_pagination a").click(function(event) {
 
       event.preventDefault();
-      var parent = $(this).closest("#review_slider");
 
+      var parent = $(this).closest("#review_slider");
       var currentHeight = $(parent).height() + "px";
 
       parent.css("height", currentHeight);
@@ -32,45 +68,16 @@ $(document).ready(function() {
       var slideTarget = $("#review_slider .slider__slide").get(count);
 
       if ( !$(this).hasClass("active") ) {
+
         $(paginationLinks).removeClass("active");
         $(this).addClass("active");
         console.log( $.inArray( this, paginationLinks ) );
 
-        $.when( $(slides).hide("fast") ).done( function() {$(slideTarget).fadeIn("fast")} );
-
-      }
-
-    })
-
-    $("#benefits_slider .slider_pagination a").click(function(event) {
-
-      event.preventDefault();
-
-      var parent = $(this).closest("#benefits_slider").find(".slider--benefits__table_wrapper");
-      var paginationLinks = $("#benefits_slider .slider_pagination a");
-      var count = $.inArray( this, paginationLinks );
-      var slides = $("#benefits_slider .slider__slide");
-      var width = $("#benefits_slider .slider__slide").css("width") + "px";
-      var slideTarget = $("#benefits_slider .slider__slide").get(count);
-
-      if ( !$(this).hasClass("active") ) {
-        $(paginationLinks).removeClass("active");
-        $(this).addClass("active");
-
-        if ( count == 0 ) {
-           $(parent).css({"transform":"translateX(21px)"})
-        }
-
-        if ( count == 1 ) {
-           $(parent).css({"transform":"translateX(-256px)"})
-        }
-
-        if ( count == 2 ) {
-           $(parent).css({"transform":"translateX(-533px)"})
-        }
-
-        console.log( $.inArray( this, paginationLinks ) );
-        var a ="255px"
+        $.when( $(slides).hide("fast") ).done( function() {
+          
+          $(slideTarget).fadeIn("fast")
+        
+        } );
 
       }
 
@@ -78,101 +85,81 @@ $(document).ready(function() {
 
   }
 
-  if( $(window).width() > 590 ) {
+    if( $(window).width() > 1200 ) {
 
-    $(".slider--benefits__table_wrapper").css({"transform":"translateX(0)"})
+      $(".slider_pagination--review a").removeClass("active");
+      $(".slider_pagination--review a:first-child").addClass("active");
 
-  }
+      $("#review_slider .slider__slide").removeClass("active").hide();
+      $("#review_slider .slider__slide:first-child").addClass("active").show();
 
-  if( $(window).width() > 1200 ) {
+      $("#review_slider .slider_controls").unbind();
+      
+      $(".slider_controls").click(function(event) {
 
-    $("#review_slider .slider_controls").unbind();
-    
-    $(".slider_controls").click(function(event) {
+        event.preventDefault();
 
-      event.preventDefault();
+        var parent = $(this).closest("#review_slider");
+        var currentHeight = $(parent).height() + "px";
 
-      var parent = $(this).closest("#review_slider");
+        parent.css("height", currentHeight);
 
-      var currentHeight = $(parent).height() + "px";
+        var prevBtn = $(parent).find(".slider_controls--prev");
+        var nextBtn = $(parent).find(".slider_controls--next");
+        var slides = $("#review_slider .slider__slide");
+        var activeSlide = $("#review_slider .slider__slide.active")[0];
+        var activeSlidePosition = +$.inArray( activeSlide, slides );
 
-      parent.css("height", currentHeight);
+        if ( $(this).hasClass("slider_controls--next") ) {
 
-      console.log( currentHeight );
+          if ( activeSlidePosition < slides.length - 1 ) {
 
-      var prevBtn = $(parent).find(".slider_controls--prev");
-      var nextBtn = $(parent).find(".slider_controls--next");
+            $(prevBtn).removeClass("disabled");
 
-      var slides = $("#review_slider .slider__slide");
-      var activeSlide1 = $("#review_slider .slider__slide.active")[0]
-      var activeSlide = +$.inArray( activeSlide1, slides );
+            var target = $(slides).get(activeSlidePosition + 1);
 
-      if ( $(this).hasClass("slider_controls--next") ) {
+            $.when( $(activeSlide).removeClass("active").fadeOut("fast") ).done(function() { 
+            
+              $(target).fadeIn("fast").addClass("active")} 
+            
+            );
 
-            if ( activeSlide < slides.length - 1 ) {
+            if ( activeSlidePosition == slides.length - 2 ) {
+              $(this).addClass("disabled");  
+            }
+          }
 
-              console.log(activeSlide);
-              console.log(slides.length);
+        }
 
-              $(prevBtn).removeClass("disabled");
+        if ( $(this).hasClass("slider_controls--prev") ) {
 
-              var target = $(slides).get(activeSlide + 1);
+          if ( activeSlidePosition !=0 ) {
 
-              $.when( $(activeSlide1).removeClass("active").fadeOut("fast") ).done(function() { 
-              
-                $(target).fadeIn("fast").addClass("active")} 
-              
-              );
+            $(nextBtn).removeClass("disabled");
 
-              if ( activeSlide == slides.length - 2 ) {
-                $(this).addClass("disabled");  
-              }
+            var target = $(slides).get(activeSlidePosition - 1);
+
+            $.when( $(activeSlide).removeClass("active").fadeOut("fast") ).done( function() {$(target).fadeIn("fast").addClass("active")}  );
+
+            if ( activeSlidePosition == 1) {
+              $(this).addClass("disabled");
             }
 
-      }
+          }
 
-      if ( $(this).hasClass("slider_controls--prev") ) {
+        }
 
-            if ( activeSlide !=0 ) {
+      })
 
-              console.log(activeSlide);
-              console.log(slides.length);
-
-              $(nextBtn).removeClass("disabled");
-
-              var target = $(slides).get(activeSlide - 1);
-
-              $.when( $(activeSlide1).removeClass("active").fadeOut("fast") ).done( function() {$(target).fadeIn("fast").addClass("active")}  );
-
-              if ( activeSlide == 1) {
-                $(this).addClass("disabled");
-              }
-
-            }
-
-      }
-
-    })
+    }
 
   }
 
-  }
+  slidersInit();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $(window).resize(function() {
+    slidersInit();
+  })
 
   $("#toggler").on("tap", function(event) {
     event.preventDefault();
